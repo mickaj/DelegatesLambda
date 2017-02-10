e510d478c2010bfeaef3280200cb709343a5534c
Mic Kajzer <mickaj@mkajzer.pl>
2017-02-10 22:05:04 +0000

    events

---
+++ DelegatesLambdas/Events/Wallet.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    class Wallet
+    {
+        private string owner;
+        public string Owner
+        {
+            get
+            {
+                return owner;
+            }
+            set
+            {
+                owner = value;
+            }
+        }
+
+        private int money;
+        public int Money
+        {
+            get
+            {
+                return money;
+            }
+            set
+            {
+                money = value;
+            }
+        }
+
+        public Wallet(string name)
+        {
+            Owner = name;
+            Money = 0;
+        }
+
+    }
+}
