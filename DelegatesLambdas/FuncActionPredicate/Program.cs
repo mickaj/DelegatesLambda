a57b6ce65bbc126f5d4553c38e854e73b61f5888
Mic Kajzer <mickaj@mkajzer.pl>
2017-02-10 18:39:24 +0000

    func, action, predicate

---
+++ DelegatesLambdas/FuncActionPredicate/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncActionPredicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Func<T,R> - generic delegate that takes T type argument and returns R type value
+            Func<string, int> GetLength = (string input) => input.Length;
+            int result = GetLength("Text you want to get length");
+            Console.WriteLine(result);
+
+            Console.WriteLine("-----------------------");
+
+            //Action<T> - generic delegate that takes T type argument and doesn't return anything
+            Action<string> WriteBackwards = (string text) =>
+                                                           {
+                                                               char[] stringChars = text.ToCharArray();
+                                                               Array.Reverse(stringChars);
+                                                               Console.WriteLine( new string(stringChars));
+                                                           };
+            WriteBackwards("Action delegate");
+
+            Console.WriteLine("-----------------------");
+
+            //Predicate<T> - generic delegate that checks a specified condition and returns boolean
+            Predicate<int> IsEven = (int input) => input % 2 == 0;
+            Console.WriteLine("5 is even: {0}", IsEven(5));
+            Console.WriteLine("12 is even: {0}", IsEven(12));
+
+            //Predicates can be used to eleminate specified items in collections (for instance in Lists);
+            List<int> Numbers = new List<int> { 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+            Console.Write("\nInitial content of the list: ");
+            foreach(int i in Numbers)
+            {
+                Console.Write("{0} ", i);
+            }
+            //removing all even numbers from the list using IsEven predicate
+            Numbers.RemoveAll(IsEven);
+            //List<T>.RemoveAll(Predicate<T> match) removes all items that returns true value when used with the predicate 
+            Console.Write("\nContent of the list after filtering: ");
+            foreach(int i in Numbers)
+            {
+                Console.Write("{0} ", i);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ DelegatesLambdas/Lambda/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    class Program
+    {
+        delegate int intDelegate(int arg);
+        delegate void voidDelegate(string arg);
+        static void Main(string[] args)
+        {
+            //anonymous method (pre-Lambda)
+            //anonymous method that returns square value of an int
+            intDelegate intInstance = delegate (int input)
+                                        {
+                                            return input*input;
+                                        };
+            int value = 5;
+            Console.WriteLine("Square value of 5: {0}", intInstance(value));
+
+            Console.WriteLine("---------------------------");
+
+            //anonymous method (pre-Lambda)
+            //anonymous method that doesn't return any value but prints given value to console
+            voidDelegate voidInstance = delegate (string text)
+                                        {
+                                            Console.WriteLine(text);
+                                        };
+            voidInstance("Given text");
+
+            Console.WriteLine("---------------------------");
+
+            //delagate with Lambda expresion that returns int
+            intDelegate intLambda = (int input) => input * input;
+            value = 6;
+            Console.WriteLine("Square value of 5: {0}", intLambda(value));
+
+            Console.WriteLine("---------------------------");
+
+            //delegate with extended Lambda expresion that returns int
+            intDelegate intExtLambda = (int input) =>
+                                                    {
+                                                        int buffer;
+                                                        buffer = input * input;
+                                                        buffer = (int)Math.PI * buffer;
+                                                        return buffer;
+                                                    };
+            Console.WriteLine("Processed value of given input: {0}", intExtLambda(value));
+
+            Console.WriteLine("---------------------------");
+
+            //delgate with Lambda expression that returns nothing
+            voidDelegate voidLambda = (string text) => Console.WriteLine(text);
+            voidLambda("Given text");
+
+            Console.WriteLine("---------------------------");
+
+            //delegate with extended Lambda expression that return nothing
+            voidDelegate voidExtLambda = (string text) =>
+                                                        {
+                                                            Console.WriteLine("Given text: {0}", text);
+                                                            Console.WriteLine("Length: {0}", text.Length);
+                                                            Console.WriteLine("Capitalizad text: {0}", text.ToUpper());
+
+                                                        };
+            voidExtLambda("input text_XXX");
+             
+
+        }
+    }
+}
