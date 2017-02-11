b3c42abb36bad7256eaedf2780592d8ee23c2f4c
Mic Kajzer <mickaj@mkajzer.pl>
2017-02-11 23:31:34 +0000

    events completed

--- DelegatesLambdas/Events/MainWindow.xaml.cs
+++ DelegatesLambdas/Events/MainWindow.xaml.cs
@@ -24,5 +24,59 @@ namespace Events
         {
             InitializeComponent();
         }
+
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Wallet thisWallet = this.Resources["myWallet"] as Wallet;
+            Button buttonClicked = sender as Button;
+            thisWallet.Money += int.Parse(buttonClicked.Content.ToString());
+            walletMoney.Text = thisWallet.Money.ToString();
+        }
+
+        private void initializeOwner_Click(object sender, RoutedEventArgs e)
+        {
+            if(ownerName.Text != "")
+            {
+                Wallet thisWallet = this.Resources["myWallet"] as Wallet;
+                thisWallet.Owner = ownerName.Text;
+                //adding method from MainWindow class that will execute when the event occurs
+                thisWallet.OnGotSomeMoney += ThisWallet_OnGotSomeMoney;
+                List<Button> buttons = new List<Button> { Add1, Add2, Add5, Add10, Add20, Add50, Add100, Add200 };
+                foreach(Button btn in buttons)
+                {
+                    btn.IsEnabled = true;
+                }
+                ownerName.IsEnabled = false;
+                initializeOwner.IsEnabled = false;
+            }
+            else
+            {
+                MessageBox.Show("Provide owner name!","Missing owner name");
+            }
+
+        }
+
+        private void ThisWallet_OnGotSomeMoney(object sender, EventArgs e)
+        {
+            Wallet thisWallet = sender as Wallet;
+            MoneyEventArgs args = e as MoneyEventArgs;
+            string _name = thisWallet.Owner;
+            int _cash = args.Money;
+            int _crossed = args.ValueCrossed;
+            MessageBox.Show("You have reached "+_crossed+" in your wallet!\nTotal amount in your wallet is "+_cash+"!", "Kudos " + _name+"!");
+        }
+
+        private void ownerName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox thisTextBox = sender as TextBox;
+            if(thisTextBox.Text != "")
+            {
+                initializeOwner.IsEnabled = true;
+            }
+            else
+            {
+                initializeOwner.IsEnabled = false;
+            }
+        }
     }
 }
--- DelegatesLambdas/Events/Wallet.cs
+++ DelegatesLambdas/Events/Wallet.cs
@@ -6,8 +6,16 @@ using System.Threading.Tasks;
 
 namespace Events
 {
-    class Wallet
+    public class Wallet
     {
+        //declaring delegate signature that will be used for event handler
+        public delegate void GotSomeMoney(object sender, EventArgs e);
+
+        //declaring an event
+        public event GotSomeMoney OnGotSomeMoney;
+
+        //booleans used to help with firing events
+        private bool reached100, reached500, reached1000, reached2000 = false;
         private string owner;
         public string Owner
         {
@@ -31,14 +39,76 @@ namespace Events
             set
             {
                 money = value;
-            }
+                //creating circumstances in which the event will be fired
+                if((money >= 2000)&&(!reached2000))
+                {
+                    FireEvent(2000);
+                    reached2000 = true;
+                }
+                else
+                {
+                    if((money >= 1000)&&(!reached1000))
+                    {
+                        FireEvent(1000);
+                        reached1000 = true;
+                    }
+                    else
+                    {
+                        if((money >= 500)&&(!reached500))
+                        {
+                            FireEvent(500);
+                            reached500 = true;
+                        }
+                        else
+                        {
+                            if((money >= 100)&&(!reached100))
+                            {
+                                FireEvent(100);
+                                reached100 = true;
+                            }
+                        }
+                    }
+                }
+            }       
         }
 
-        public Wallet(string name)
+        public Wallet()
         {
-            Owner = name;
+            Owner = "";
             Money = 0;
         }
+   
+        //declaring a private method which will be used to fire the event
+        //it creates an instance of event arguments class (in this case MoneyEventArgs)
+        //it checks if there are any subscriptions to the event before firing it 
+        private void FireEvent(int valueCrossed)
+        {
+            MoneyEventArgs e = new MoneyEventArgs();
+            e.Money = Money;
+            e.ValueCrossed = valueCrossed;
+            if(OnGotSomeMoney != null) //checking if there are subscriptions
+            {
+                OnGotSomeMoney(this, e); //if there are the event will be fired with 'this' object and e EventArgs as arguments
+            }
+            //no action if there are no subscriptions
+        }
 
+    }
+
+    //declaring class for event arguments. This class must be derived form EventArgs class
+    //more on EventArgs: https://msdn.microsoft.com/en-us/library/system.eventargs(v=vs.110).aspx
+    public class MoneyEventArgs :EventArgs
+    {
+        public int Money
+        {
+            get;
+            set;
+        }
+
+        public int ValueCrossed
+        {
+            get;
+            set;
+        }
     }
 }
