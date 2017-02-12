43b3b6cb07b5b35b58402921373ccb13fa94ab4e
Mic Kajzer <mickaj@mkajzer.pl>
2017-02-12 19:28:00 +0000

    call backs

---
+++ DelegatesLambdas/DelegateCallbacks/Calculation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DelegateCallbacks
+{
+    public class Calculation
+    {
+        private int cycles;
+        public int Cycles
+        {
+            get
+            {
+                return cycles;
+            }
+            set
+            {
+                cycles = value;
+            }
+        }
+
+        //default constructor
+        public Calculation(int _cycles)
+        {
+            Cycles = _cycles;
+        }
+
+        //running this method will call back to main each iteration of for loop
+        public void Process(CallBack callBackMethod)
+        {
+            for(int i = 1; i <= cycles; i++)
+            {
+                //immitation of some calculations that takes 1/10 of second
+                Thread.Sleep(100); //in miliseconds
+                //point of calling back
+                callBackMethod(i);
+            } 
+        }
+
+        //delegate that determines methods of what signatures can be used as call backs
+        public delegate void CallBack(int i);  
+    }
+}
---
+++ DelegatesLambdas/DelegateCallbacks/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace DelegateCallbacks
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow :Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void runProcess_Click(object sender, RoutedEventArgs e)
+        {
+            int itr;
+            if(!int.TryParse(iterationsQTY.Text, out itr))
+            {
+                MessageBox.Show("Cannot convert input itereation number to integer!", "Upsss...");
+            }
+            else
+            {
+                runProcess.IsEnabled = false;
+                iterationsQTY.IsEnabled = false;
+                runProcess.IsEnabled = false;
+                progress.Maximum = itr;
+                progress.IsEnabled = true;
+                progress.Value = 0;
+                //creating new instance of Calculation class
+                Calculation thisCalc = new Calculation(itr);
+                //creating new task for the Process method to keep the UI responsive
+                Task processTask = new Task(() => thisCalc.Process(UpdateUI));
+                processTask.Start();
+                // continueTask specifies what happens when processTask is completed
+                Task continueTask = processTask.ContinueWith((a) =>
+                {
+                    MessageBox.Show("Process compeleted after " + itr + " iterations.", "Process finished");
+                });
+            }
+        }
+
+        //call back method that gets current iteration in Process method and updates the UI
+        public void UpdateUI(int i)
+        {
+            //updating the UI is achieved through dispatcher to allow processTask to update values in root thread
+            this.Dispatcher.Invoke(() =>
+            {
+                iterationNo.Text = i.ToString();
+                progress.Value = i;
+                if(i == int.Parse(iterationsQTY.Text))
+                {
+                    iterationsQTY.IsEnabled = true;
+                    runProcess.IsEnabled = true;
+                    progress.IsEnabled = false;
+                    runProcess.IsEnabled = true;
+                }
+            });
+        }
+    }
+}
